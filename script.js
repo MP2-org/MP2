00a3245264d7fa34eaf8d25a6a773a7aa6a6351e
ianafable <afable.marcian@gmail.com>
2023-02-16 23:24:58 +0800

    adding contents

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.querySelector('#loader').classList.add('loading-done');
+  }, 6000);
+});
+
+
 const menuIcon = document.querySelector(".menu-icon");
 const menu = document.querySelector(".menu");
 
