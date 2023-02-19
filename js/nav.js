e75994e95e37b92d4bbcdcbb3b09ab0a056a3585
orling09 <orlandjude@gmail.com>
2023-02-19 18:42:26 +0800

    added contents on contact.html file, edited CSS and JS file

--- js/nav.js
+++ js/nav.js
@@ -46,3 +46,33 @@ function openPopup() {
 function closePopup() {
   popup.classList.remove("open-popup");
 }
+
+// faq
+
+let toggles = document.getElementsByClassName("toggle");
+let contentDiv = document.getElementsByClassName("content5");
+let icons = document.getElementsByClassName("icon5");
+
+for (let i = 0; i < toggles.length; i++) {
+  toggles[i].addEventListener("click", () => {
+    if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+      contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+      toggles[i].style.color = "#FFFFFF";
+      icons[i].classList.remove("bi-plus");
+      icons[i].classList.add("bi-dash");
+    } else {
+      contentDiv[i].style.height = "0px";
+      toggles[i].style.color = "#000000";
+      icons[i].classList.remove("bi-dash");
+      icons[i].classList.add("bi-plus");
+    }
+    for (let j = 0; j < contentDiv.length; j++) {
+      if (j !== i) {
+        contentDiv[j].style.height = "0px";
+        toggles[j].style.color = "#000000";
+        icons[j].classList.remove("bi-dash");
+        icons[j].classList.add("bi-plus");
+      }
+    }
+  });
+}
